330a3975fad5a352f9240a9e775fab8c7c970880
BiancaBlackwell <bianca6298@ymail.com>
2021-05-22 21:04:01 -0700

    test chat bot, multi user but same output

--- main.py
+++ main.py
@@ -1,47 +1,22 @@
-from flask import Flask, render_template, session, copy_current_request_context
-from flask_socketio import SocketIO, emit, disconnect
-from threading import Lock
 
+from flask import Flask, render_template
+from flask_socketio import SocketIO
 
-async_mode = None
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret'
-socket_ = SocketIO(app, async_mode=async_mode)
-thread = None
-thread_lock = Lock()
-
+app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
+socketio = SocketIO(app)
 
 @app.route('/')
-def index():
-    return render_template('session.html', async_mode=socket_.async_mode)
+def sessions():
+    return render_template('session.html')
 
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!!!')
 
-@socket_.on('my_event', namespace='/test')
-def test_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']})
-
-
-@socket_.on('my_broadcast_event', namespace='/test')
-def test_broadcast_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']},
-         broadcast=True)
-
-
-@socket_.on('disconnect_request', namespace='/test')
-def disconnect_request():
-    @copy_current_request_context
-    def can_disconnect():
-        disconnect()
-
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': 'Disconnected!', 'count': session['receive_count']},
-         callback=can_disconnect)
-
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageReceived)
 
 if __name__ == '__main__':
-	socket_.run(app)
+    socketio.run(app, debug=True)
