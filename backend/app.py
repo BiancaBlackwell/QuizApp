5998718876b1ea4cb3b1be034a151d507b1b025b
AustinMager <austinmagerd@gmail.com>
2021-05-28 14:36:11 -0500

    debugged and tested crate user, create room, join room

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 import sqlite3
 import random
 import uuid
+import string
 
 """How to use: flask run"""
 
@@ -9,82 +10,151 @@ import uuid
 app = Flask(__name__)
 DATABASE = 'trivia.db'
 
+@app.route('/')
+def index():
+	return 'Hello World'
+
+
+
+#####~~~~~~~~~~~~~~~ Questions ~~~~~~~~~~~~~~~#####
+
+
+@app.route('/backend/getQuestion')
+def getQuestion():
+	size = 0
+	cur = get_db().cursor()
+	for row in cur.execute('SELECT COUNT(id) FROM complete'):
+		size = row[0]
+	return getFromComplete(random.randint(1, size-1), cur)
+
+@app.route('/backend/getQuestion/<category>')
+def getQuestionWithCategory(category):
+	size = 0
+	cur = get_db().cursor()	
+	for row in cur.execute('SELECT COUNT(id) FROM ' + category):
+		size = row[0]
+	for row in cur.execute('SELECT * FROM ' + category + ' WHERE id = ' + str(random.randint(1, size - 1))):
+		cur.close()
+		return getFromComplete(row[0], cur)
+	cur.close()
+	return "ToDo " + str(category);
+
+
+
+
+#####~~~~~~~~~~~~~~~ Rooms and Userss ~~~~~~~~~~~~~~~#####
+
+
+@app.route('/backend/createUser')
+def createUser():
+	#Generate UserID (Randomly)
+	userid = str(uuid.uuid1())
+	#Save it in the users table of the DB
+	cur = get_db().cursor()
+	query = f'INSERT INTO users VALUES ("{userid}", null);'
+	cur.execute(query)
+
+	cur.close()
+	return userid
+
+#Add user to room or generate room
+@app.route('/backend/joinRoom/<userid>=<roomid>')
+def joinRoom(userid, roomid):
+	if not verifyUser:
+		return "False"
+	if not verifyRoom:
+		return "False"
+
+	db = get_db()
+	cur = db.cursor()
+	query = f'UPDATE users SET roomid = "{roomid}" WHERE userid = "{userid}";'
+	cur.execute(query)
+	query = f'UPDATE rooms SET partysize = partysize + 1 WHERE roomid = "{roomid}";'
+	cur.execute(query)
+
+	cur.close()
+	db.commit()
+	return "True"
+
+
+@app.route('/backend/createRoom')
+def createRoom():
+
+	#create a unique roomid
+	roomid = getRandomString(8)
+	while verifyRoom(roomid):
+		roomid = getRandomString(8)
+
+	#add room to database
+	db = get_db()
+	cur = db.cursor()
+	query = f'INSERT INTO rooms VALUES ("{roomid}", 0);'
+	cur.execute(query)
+
+	# close cursor and commit change
+	cur.close()
+	db.commit()
+	return roomid
+
+@app.route('/backend/removeRoom/<roomid>')
+def removeRoom(roomid):
+
+	#verify the room exists
+	if not verifyRoom(roomid):
+		return "False"
+
+	#remove room to database
+	db = get_db()
+	cur = db.cursor()
+	query = f'DELETE FROM rooms WHERE roomid = "{roomid}";'
+	cur.execute(query)
+
+	# close cursor and commit change
+	cur.close()
+	db.commit()
+	return "True"
+
+
+
+#####~~~~~~~~~~~~~~~ Helper Methods ~~~~~~~~~~~~~~~#####
+
+
 def get_db():
 	db = getattr(Flask, '_database', None)
 	if db is None:
 		db = Flask._database = sqlite3.connect(DATABASE, check_same_thread=False)
 	return db
 
-@app.route('/')
-def index():
-	return 'Hello World'
-
-@app.route('/backend/getQuestion')
-def getQuestion():
-    size = 0
-    cur = get_db().cursor()	
-    for row in cur.execute('SELECT COUNT(id) FROM complete'):
-        size = row[0]
-    return getFromComplete(random.randint(1, size-1), cur)
-
-@app.route('/backend/getQuestion/<category>')
-def getQuestionWithCategory(category):
-    size = 0
-    cur = get_db().cursor()	
-    for row in cur.execute('SELECT COUNT(id) FROM ' + category):
-        size = row[0]
-    for row in cur.execute('SELECT * FROM ' + category + ' WHERE id = ' + str(random.randint(1, size - 1))):
-        return getFromComplete(row[0], cur)
-    return "ToDo " + str(category);
-
-@app.route('/backend/getUserid')
-def getUserid():
-	#Generate UserID (Randomly)
-	userid = uuid.uuid1()
-	#Save it in the users table of the DB
-	cur = get_db().cursor()
-	query = f'INSERT INTO {users} VALUES ({userid}, null);'
-    cur.execute(query)
-	return userid
-
-#Add user to room or generate room
-@app.route('/backend/joinRoom')
-def joinRoom(userid, roomid):
-	if not verifyUser:
-		return False
-	if not verifyRoom:
-		return False
-	cur = get_db().cursor()
-	query = f'UPDATE users SET roomid = {roomid} WHERE userid = {userid};'
-	cur.execute(query)
-	return True
-
-@app.route('/backend/createRoom')
-def createRoom(userid):
-
-
 def getFromComplete(id, cur):
-    for row in cur.execute('SELECT * FROM complete WHERE id = ' + str(id)):
-        cur.close()
-        return str(row)
+	for row in cur.execute('SELECT * FROM complete WHERE id = ' + str(id)):
+		cur.close()
+		return str(row)
 
 def verifyUser(userid):
 	cur = get_db().cursor()
-	query = f'SELECT COUNT(userid) FROM users WHERE userid = {userid};'
-	count = cur.execute(query)
+	query = f'SELECT COUNT(userid) FROM users WHERE userid = "{userid}";'
+	cur.execute(query)
+	count = cur.fetchone()[0]
+	cur.close()
 	if count > 0:
 		return True
-	else
-		return False
+	return False
 
 def verifyRoom(roomid):
 	cur = get_db().cursor()
-	query = f'SELECT COUNT(roomid) FROM rooms WHERE roomid = {roomid};'
-	count = cur.execute(query)
+	query = f'SELECT COUNT(roomid) FROM rooms WHERE roomid = "{roomid}";'
+	cur.execute(query)
+	count = cur.fetchone()[0]
+	cur.close()
 	if count > 0:
 		return True
-	else
-		return False 
+	return False 
+
+def getRandomString(length):
+    letters = string.ascii_lowercase + string.ascii_uppercase
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
